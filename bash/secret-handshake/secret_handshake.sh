19e436c0cf34e84dd2e9acc314c9cc9e453ff69a
Alfonso Alba García <alfonso@alfonsoalba.com>
2020-12-26 20:03:33 +0100

    bash/secret-handshake: Extract method output.

--- bash/secret-handshake/secret_handshake.sh
+++ bash/secret-handshake/secret_handshake.sh
@@ -4,28 +4,17 @@
 #D2B=({0..1}{0..1}{0..1}{0..1}{0..1})
 
 DATA=(1 2 4 8)
+SHOULD_REVERSE="false"
 
-main () {
-  local encoded=()
-  local output=""
-
-  # We need to use double quotes so elements with multiple words separated with spaces count as one
-  for I in "${DATA[@]}"; do
-    case  $(( $I & $1 )) in
-      1 ) encoded+=('wink') ;;
-      2 ) encoded+=('double blink') ;;
-      4 ) encoded+=('close your eyes') ;;
-      8 ) encoded+=('jump') ;;
-      * ) ;;
-    esac 
-  done
+function output() {
+  local encoded=("$@")
 
   if [ "${#encoded[@]}" = "0" ]; then
     echo ""
   elif [ "${#encoded[@]}" = "1" ]; then
     echo "${encoded[0]}"
   else
-    if [ $(( 16 & $1 )) = "0" ]; then  
+    if [ "$SHOULD_REVERSE" = "false" ]; then  
       for I in "${encoded[@]}"; do
         output+="$I,"
       done
@@ -43,6 +32,27 @@ main () {
     echo $output
 
   fi
+
+}
+
+main () {
+  local encoded=()
+  local output=""
+  
+  [[ $(( 16 & $1 )) != "0" ]] && SHOULD_REVERSE="true"
+
+  # We need to use double quotes so elements with multiple words separated with spaces count as one
+  for I in "${DATA[@]}"; do
+    case  $(( $I & $1 )) in
+      1 ) encoded+=('wink') ;;
+      2 ) encoded+=('double blink') ;;
+      4 ) encoded+=('close your eyes') ;;
+      8 ) encoded+=('jump') ;;
+      * ) ;;
+    esac 
+  done
+
+  output "${encoded[@]}"
 }
 
 # call main with all of the positional arguments
