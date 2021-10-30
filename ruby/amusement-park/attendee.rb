8ffe46ce8310eb89f50ddff344be2228cbd20413
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-10-30 12:26:52 +0200

    ruby/amusement-park: first working solution.

---
+++ ruby/amusement-park/attendee.rb
@@ -0,0 +1,17 @@
+class Attendee
+  attr_reader :height
+  attr_reader :pass_id
+
+  def initialize(height)
+    @height = height
+    @pass_id = nil
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+end
