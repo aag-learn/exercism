96a9dd7bd9cc0bf1625b655e8c670c144ce8f9f4
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-01 07:41:06 +0100

    ruby/simple-calculator: second iteration

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -7,23 +7,13 @@ class SimpleCalculator
 
   def self.calculate(first_operand, second_operand, operation)
     raise ArgumentError unless first_operand.is_a?(Numeric) && second_operand.is_a?(Numeric)
-    case operation
-    when '+' then self.format(first_operand, second_operand, operation, first_operand + second_operand)
-    when '-' then self.format(first_operand, second_operand, operation, first_operand - second_operand)
-    when '*' then self.format(first_operand, second_operand, operation, first_operand * second_operand)
-    when '/' then 
-      if second_operand == 0
-        "Division by zero is not allowed."
-      else
-        self.format(first_operand, second_operand, operation, first_operand / second_operand)
-      end
-    else raise UnsupportedOperation.new
+    raise UnsupportedOperation unless ALLOWED_OPERATIONS.include? operation
+
+    begin
+      result = first_operand.send(operation, second_operand)
+      "#{first_operand} #{operation} #{second_operand} = #{result.to_s}"  
+    rescue ZeroDivisionError => e
+      "Division by zero is not allowed."
     end
-  end
-
-  private 
-
-  def self.format first_operand, second_operand, operation, result
-    "#{first_operand.to_s} #{operation.to_s} #{second_operand.to_s} = #{result.to_s}"
   end
 end
