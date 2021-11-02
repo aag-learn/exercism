72d86f7d2bea08fb51986374b8da9e418627b904
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-02 06:09:55 +0100

    ruby/moviegoer: first working solution

---
+++ ruby/moviegoer/moviegoer.rb
@@ -0,0 +1,25 @@
+# This is a custom exception that you can use in your code
+class NotMovieClubMemberError < RuntimeError
+end
+
+class Moviegoer
+  attr_reader :age, :member
+
+  def initialize(age, member: false)
+    @age = age
+    @member = member
+  end
+
+  def ticket_price
+    age >= 60 ? 10 : 15
+  end
+
+  def watch_scary_movie?
+    age >= 18
+  end
+
+  # Popcorn is 🍿
+  def claim_free_popcorn!
+    member ? "🍿" : (raise NotMovieClubMemberError.new)
+  end
+end
