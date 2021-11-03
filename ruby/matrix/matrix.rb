81a502d53739d5e4cf85038d9b2b137bd4b315d8
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-03 07:08:36 +0100

    ruby/matrix: second iteration. Use lines method

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,7 +2,7 @@ class Matrix
     attr_reader :columns, :rows
     
     def initialize data
-        @rows = data.split("\n").map{|row| row.strip.split.map(&:to_i)}
+        @rows = data.lines.map{|row| row.strip.split.map(&:to_i)}
         @columns = rows.transpose
     end
 end
