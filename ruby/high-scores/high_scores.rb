9433c241d88a3188827c2e3ba9e9125fd6fef921
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-03 06:29:33 +0100

    ruby/high-scopres: second iteration. Use instance variables and precalculations in the constructor.

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,30 +1,15 @@
 class HighScores
 
-    attr_reader :scores
+    attr_reader :latest, :personal_best, :personal_top_three, :scores
     
     def initialize scores
-        self.scores = scores
-        self.sorted_scores = scores.sort{|a,b| b<=>a}
-    end
-
-    def latest
-        scores.last
-    end
-
-    def personal_best
-        scores.max
-    end
-
-    def personal_top_three
-        sorted_scores.length > 3 ? sorted_scores[0..2]  :  sorted_scores 
+        @scores = scores
+        @latest = scores.last
+        @personal_top_three = scores.max(3)
+        @personal_best = @personal_top_three.first
     end
 
     def latest_is_personal_best?
         personal_best == latest
     end
-
-    private
-
-    attr_writer :scores
-    attr_accessor :sorted_scores
 end
