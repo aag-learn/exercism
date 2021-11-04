e17cfb4bde39b401be35a56500b516994ca9d11d
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-04 08:10:01 +0100

    ruby/series: second iteration. Use array methods instead of ranges.

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -6,9 +6,7 @@ class Series
 
     def slices num_items
         raise ArgumentError.new if num_items > series.length
-        (0..(series.length - num_items)).map do |i|
-            series[i..(i + num_items -1)]
-        end
+        series.scan(/\w/).each_cons(num_items).map(&:join)
     end
 
     private 
