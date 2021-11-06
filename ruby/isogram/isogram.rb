b9e56a0674ab6e3e464fd4e4371b95613984278c
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-06 08:53:41 +0100

    ruby/isogram: fourth iteration. Use optimized solution.

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,25 +1,25 @@
 module Isogram
     def self.isogram? word 
 
+        # First iteration
         # word.empty? || word.downcase.gsub(/[-\s]/,'').chars.reduce({}){|sum, char| sum[char] ? sum[char] += 1 : sum[char] = 1; sum}.values.max == 1
 
         # Solution using uniq
-        chars = word.downcase.scan(/[a-z]/)
-        chars.count == chars.uniq.count
+        # chars = word.downcase.scan(/[a-z]/)
+        # chars.count == chars.uniq.count
 
-        # Improve performance: we can stop  processing the string when we find the first repetition
-        # return true if word.empty?
-        # is_isogram = true
-        # word.downcase.gsub(/[-\s]/,'').chars.reduce({}) do |sum, char| 
-        #     if sum[char] 
-        #         is_isogram = false
-        #         break
-        #     else 
-        #         sum[char] = true
-        #     end
-        #     sum
-        # end
-        # is_isogram
-
+        # Improve performance: we can stop processing the string when we find the first repetition
+        return true if word.empty?
+        is_isogram = true
+        word.downcase.gsub(/[-\s]/,'').chars.reduce({}) do |sum, char| 
+            if sum[char] 
+                is_isogram = false
+                break
+            else 
+                sum[char] = true
+            end
+            sum
+        end
+        is_isogram
     end
 end
