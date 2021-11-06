eefc64bee064c8aaf61f9344e4058714970afe3e
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-06 08:37:25 +0100

    ruby/isogram: second iteration. Improve performance.

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,5 +1,19 @@
 module Isogram
     def self.isogram? word 
-        word.empty? || word.downcase.gsub(/[-\s]/,'').chars.reduce({}){|sum, char| sum[char] ? sum[char] += 1 : sum[char] = 1; sum}.values.max == 1
+        # Improve performance: we can stop  processing the string when we find the first repetition
+        # word.empty? || word.downcase.gsub(/[-\s]/,'').chars.reduce({}){|sum, char| sum[char] ? sum[char] += 1 : sum[char] = 1; sum}.values.max == 1
+
+        return true if word.empty?
+        is_isogram = true
+        word.downcase.gsub(/[-\s]/,'').chars.reduce({}) do |sum, char| 
+            if sum[char] 
+                is_isogram = false
+                break
+            else 
+                sum[char] = true
+            end
+            sum
+        end
+        is_isogram
     end
 end
