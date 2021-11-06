ca06de7f623bbd7b5ec0dcad7c0611cd827a89ac
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-06 08:51:01 +0100

    ruby/isogram: third iteration. Use uniq.

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,19 +1,25 @@
 module Isogram
     def self.isogram? word 
-        # Improve performance: we can stop  processing the string when we find the first repetition
+
         # word.empty? || word.downcase.gsub(/[-\s]/,'').chars.reduce({}){|sum, char| sum[char] ? sum[char] += 1 : sum[char] = 1; sum}.values.max == 1
 
-        return true if word.empty?
-        is_isogram = true
-        word.downcase.gsub(/[-\s]/,'').chars.reduce({}) do |sum, char| 
-            if sum[char] 
-                is_isogram = false
-                break
-            else 
-                sum[char] = true
-            end
-            sum
-        end
-        is_isogram
+        # Solution using uniq
+        chars = word.downcase.scan(/[a-z]/)
+        chars.count == chars.uniq.count
+
+        # Improve performance: we can stop  processing the string when we find the first repetition
+        # return true if word.empty?
+        # is_isogram = true
+        # word.downcase.gsub(/[-\s]/,'').chars.reduce({}) do |sum, char| 
+        #     if sum[char] 
+        #         is_isogram = false
+        #         break
+        #     else 
+        #         sum[char] = true
+        #     end
+        #     sum
+        # end
+        # is_isogram
+
     end
 end
