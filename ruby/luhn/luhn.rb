09a75e8121fe69d3b6421ea9eb86f7c2f8523cb2
Alfonso Alba <alfonso@alfonsoalba.com>
2021-11-11 08:48:28 +0100

    ruby/luhn: second iteration. Use tap and with_index.

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,17 +1,19 @@
 module Luhn
     def self.valid? input
-        return false unless input =~ /^[\d\s]{2,}$/
-        input_without_spaces = input.gsub(/\s/, '')
-        return false if input_without_spaces == "0"
 
-        doubled = input_without_spaces.chars.reverse.each_with_index.map do |i, index|
-            number = i.to_i
-            if 0 != index % 2
-                number <= 4 ? number * 2 : number * 2 - 9
-            else
-                number
-            end
-        end
+        doubled = input.
+                  gsub(/\s/, '').
+                  tap{|o| return false if o =~ /[^\d]/ || o == "0"}.
+                  chars.
+                  reverse.
+                  map.with_index do |i, index|
+                      number = i.to_i
+                      if 0 != index % 2
+                          number <= 4 ? number * 2 : number * 2 - 9
+                      else
+                          number
+                      end
+                  end
 
         doubled.sum % 10 == 0
     end
