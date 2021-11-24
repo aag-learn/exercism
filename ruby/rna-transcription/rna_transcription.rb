8eb0fd32bc79f286489577ad38e1ffcecafbdffb
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-24 17:41:23 +0100

    ruby/rna-transcript: second iteration. Use String#tr to simplify the code.

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,12 +1,6 @@
 module Complement
 
-    COMPLEMENT = {
-        'G' => 'C',
-        'C' => 'G',
-        'T' => 'A',
-        'A' => 'U'
-    }
     def self.of_dna strand 
-        strand.chars.map{|c| COMPLEMENT[c]}.join('')
+        strand.tr('GCTA', 'CGAU')
     end
 end
