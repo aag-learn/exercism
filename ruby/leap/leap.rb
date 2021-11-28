50584211d07c1c532d22d41cfc193798de580450
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-11-28 20:15:04 +0100

    ruby/leap: second iteration. Simplify expression using linear algebra.

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,7 +1,23 @@
 class Year
     def self.leap? year
-        year % 4 == 0 && ( year % 100 != 0  || ( year % 100 == 0 && year % 400 == 0 ) )    
-        # year % 4 == 0 && ! ( year % 100 == 0 && ( year % 100 != 0 || year % 400 != 0 ) )
+        # This is the expression I came up with when I solved this problem in iteration 1
+        # year % 4 == 0 && ( year % 100 != 0  || ( year % 100 == 0 && year % 400 == 0 ) )
+        #    
+        # Let's simplfy this expression using Boolena Algebra
+        # (See for example http://www.cse.psu.edu/~kxc104/class/cmpen271/13f/lec/L06NandXorGates.pdf)
+        # A = year % 4 == 0
+        # B = year % 100 == 0
+        # C = year % 400 == 0
+        #
+        # A * (B' + B*C) -> This is the expression I came up with
+        # Using that (A + B) * (A + C) = A + B*C we can rewrite (B' + B*C) as follows:
+        #
+        # A * ( (B' + B)*(B' + C) )
+        # Since B + B' = 1
+        #
+        # A * (B' + C)
+        # Which is:
+        year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0 )
     end
 end
 
