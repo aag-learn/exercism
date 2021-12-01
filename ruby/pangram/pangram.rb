e07c6b57b2d48ce0113baf078a7dc3e0880bc7db
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-12-01 21:48:46 +0100

    ruby/pangram: second iteration. Use the Enumerable#all? method.

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -3,10 +3,6 @@ require 'set'
 module Pangram
     NUMBER_OF_LETTERS = 26
     def self.pangram? text 
-        text.
-            downcase.
-            scan(/[a-z]/).
-            reduce(Set.new){|set, letter| set.add(letter); set}.
-            size == NUMBER_OF_LETTERS
+        ("a".."z").all?{|letter| text.downcase.include? letter}
     end
 end
