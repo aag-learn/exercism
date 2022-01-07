39ea717e540d718dde43e1483a5e8c6f6113cd55
Alfonso Alba García <alfonso@alfonsoalba.com>
2022-01-07 07:20:54 +0100

    ruby/difference-of-squares: second iteration. Use formulas.

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,3 +1,4 @@
+# See https://brilliant.org/wiki/sum-of-n-n2-or-n3/ for the formulas
 class Squares
     attr_reader :n
 
@@ -6,29 +7,15 @@ class Squares
     end
 
     def square_of_sum 
-        res =  0
-        return 1 if n == 1
-        if n % 2 == 0 
-            res = square_of_sum_of_even_number n
-        else
-            res = n + square_of_sum_of_even_number(n-1)
-        end
-
-        res**2
+        ((n + 1) * n / 2)**2
     end
 
     def sum_of_squares
-        (1..n).sum{|i| i**2}
+        n * (n + 1) * (2*n + 1) / 6
     end
 
     def difference
         square_of_sum - sum_of_squares
-    end
-
-    private 
-
-    def square_of_sum_of_even_number n
-        n + n/2 + n*(n/2 - 1)
     end
     
 end
