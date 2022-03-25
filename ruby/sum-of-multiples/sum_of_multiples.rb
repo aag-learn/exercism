74282aca80dcac776c51a3d220386f657c85c4b6
Alfonso Alba García <alfonso@alfonsoalba.com>
2022-03-25 17:57:19 +0100

    ruby/sum-of-multiples: Second iteration. Using "..." range operator. Add comment with clever implementation.

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -4,7 +4,14 @@ class SumOfMultiples
     end
 
     def to number
-        (1..number-1).reduce(0){|sum, number| (multiple? number) ? sum+number : sum }
+        # My implementation
+        (1...number).reduce(0){|sum, number| (multiple? number) ? sum+number : sum }
+
+        # A clever implementation taken from here https://exercism.org/tracks/ruby/exercises/sum-of-multiples/solutions/tenebrousedge
+        #
+        # multiples.flat_map do |factor|
+        #   (factor...number).step(factor).to_a
+        # end.uniq.sum
 
         # Another alternative implementation:
         #
@@ -13,6 +20,8 @@ class SumOfMultiples
         # I don't like this one because we need to find the numbers first and then
         # make a second pass to add them up. In the current implementation
         # we do everything in one pass
+
+
 
     end
 
