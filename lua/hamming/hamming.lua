ca7431bef01eafd293b985be23c06d5e776b019e
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-05-14 13:15:59 +0200

    lua/hamming: third iteration.

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,13 +1,9 @@
 local Hamming = {}
 
 function Hamming.compute(a,b)
+	if(#a ~= #b) then return -1 end
 	local distance = 0
-	if(#a ~= #b) 
-	then
-		return -1
-	end
-
-	for i =1, #a do
+	for i = 1, #a do
 		distance = distance + ( a:sub(i,i) == b:sub(i,i) and 0 or 1 )
 	end
 	return distance
