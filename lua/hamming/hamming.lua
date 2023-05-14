64b6234d24a4cbd15292f7709372fdef5a5d6960
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-05-14 12:41:46 +0200

    lua/hamming: first iteration

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,7 +1,19 @@
 local Hamming = {}
 
 function Hamming.compute(a,b)
+	local distance = 0
+	if(#a ~= #b) 
+	then
+		return -1
+	end
 
+	for i =1, #a do
+		if( a:sub(i,i) ~= b:sub(i,i) )
+		then
+			distance = distance + 1
+		end
+	end
+	return distance
 end
 
 return Hamming
