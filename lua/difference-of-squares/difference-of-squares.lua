56d89ee7046b293ae5f1e5635985bdd458cf488e
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-07-12 06:59:10 +0200

    [difference-of-squares] First iteration

---
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -0,0 +1,18 @@
+local function square_of_sum(n)
+	local sum = (n + math.pow(n, 2)) / 2
+	return math.pow(sum, 2)
+end
+
+local function sum_of_squares(n)
+	return n * (n + 1) * (2 * n + 1) / 6
+end
+
+local function difference_of_squares(n)
+	return square_of_sum(n) - sum_of_squares(n)
+end
+
+return {
+	square_of_sum = square_of_sum,
+	sum_of_squares = sum_of_squares,
+	difference_of_squares = difference_of_squares,
+}
---
+++ lua/difference-of-squares/test.lua
@@ -0,0 +1,2 @@
+local diff = require("difference-of-squares")
+diff.square_of_sum(5)
