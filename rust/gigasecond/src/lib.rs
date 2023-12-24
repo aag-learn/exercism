49b816f3c93c5fa757f1f8feb48bfd09f32238a4
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-12-24 08:04:46 +0100

    [rust/gigasecond] Second iteration using X.seconds() notation

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,9 +1,7 @@
-use time::{Duration, PrimitiveDateTime as DateTime};
+use time::ext::NumericalDuration;
+use time::PrimitiveDateTime as DateTime;
+const GIGASECOND: i64 = 1_000_000_000;
 
-const GIGASECOND: i64 = 1000000000;
-
-// Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    //todo!("What time is a gigasecond later than {start}");
-    start + Duration::seconds(GIGASECOND)
+    start + GIGASECOND.seconds()
 }
