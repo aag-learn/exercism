dffb7103024bf326da3a932eba76822eefaf8e2a
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-12-29 00:35:49 +0100

    [rust/clock] Second iteration: remove duplicated code.

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, print};
+use std::fmt::Display;
 
 #[derive(Debug)]
 pub struct Clock {
@@ -8,26 +8,8 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut clock_hours = hours;
-        let extra_hours = minutes / 60;
-        clock_hours = (clock_hours + extra_hours).rem_euclid(24);
-        let reminder_minutes = minutes - extra_hours * 60;
-        let clock_minutes = reminder_minutes.rem_euclid(60);
-        if reminder_minutes < 0 {
-            if clock_hours == 0 {
-                clock_hours = 23;
-            } else {
-                clock_hours -= 1;
-            }
-        }
-        println!("--->> arguments:     {}:{}", hours, minutes);
-        println!("--->> extra_hours:   {}", extra_hours);
-        println!("--->> clock_hours:   {}", clock_hours);
-        println!("--->> clock_minutes: {}", clock_minutes);
-        Self {
-            minutes: clock_minutes,
-            hours: clock_hours,
-        }
+        let clock = Self { hours, minutes: 0 };
+        clock.add_minutes(minutes)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
@@ -35,11 +17,6 @@ impl Clock {
         let mut clock_hours = (self.hours + extra_hours).rem_euclid(24);
         let reminder_minutes = self.minutes + minutes - extra_hours * 60;
         let clock_minutes = reminder_minutes.rem_euclid(60);
-        print!("in add_minutes");
-        println!("--->> arguments:     {}", minutes);
-        println!("--->> extra_hours:   {}", extra_hours);
-        println!("--->> clock_hours:   {}", clock_hours);
-        println!("--->> clock_minutes: {}", clock_minutes);
         if reminder_minutes < 0 {
             if clock_hours == 0 {
                 clock_hours = 23;
