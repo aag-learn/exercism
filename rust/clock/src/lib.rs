fa53f869976c60f060fe7a1c93a522481137e438
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-12-29 01:17:12 +0100

    [rust/clock] fourth iteration: only use minutes!!

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,38 +1,35 @@
 use std::fmt::Display;
 
+const MINUTES_IN_CLOCK: i32 = 24 * 60;
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let clock = Self { hours, minutes: 0 };
-        clock.add_minutes(minutes)
+        Self {
+            minutes: (hours * 60 + minutes).rem_euclid(MINUTES_IN_CLOCK),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let extra_hours = (self.minutes + minutes) / 60;
-        let mut clock_hours = (self.hours + extra_hours).rem_euclid(24);
-        let reminder_minutes = self.minutes + minutes - extra_hours * 60;
-        let clock_minutes = reminder_minutes.rem_euclid(60);
-        if reminder_minutes < 0 {
-            if clock_hours == 0 {
-                clock_hours = 23;
-            } else {
-                clock_hours -= 1;
-            }
-        }
         Self {
-            minutes: clock_minutes,
-            hours: clock_hours,
+            minutes: (self.minutes + minutes).rem_euclid(MINUTES_IN_CLOCK),
         }
+    }
+
+    fn hours(&self) -> i32 {
+        self.minutes / 60
+    }
+
+    fn display_minutes(&self) -> i32 {
+        self.minutes - self.hours() * 60
     }
 }
 
 impl Display for Clock {
     fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(fmt, "{:0>2}:{:0>2}", self.hours, self.minutes)
+        write!(fmt, "{:0>2}:{:0>2}", self.hours(), self.display_minutes())
     }
 }
