826acb22174f8569f1b7af0f2d9da681371f6c8f
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-12-31 08:20:34 +0100

    [rust/anagram] Second iteration

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,32 +1,25 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    // todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
-    let lowercase_possible_anagrams: Vec<String> = possible_anagrams
+    let lowercased_word = word.to_lowercase();
+    let sorted_word = sort_word(&lowercased_word);
+
+    possible_anagrams
         .iter()
-        .map(|item| item.to_lowercase())
-        .collect();
-    let lowercase_and_sorted_possible_anagrams: Vec<String> = lowercase_possible_anagrams
-        .iter()
-        .map(|item| {
-            let mut chars: Vec<char> = item.chars().collect();
-            chars.sort_unstable();
-            chars.into_iter().collect()
-        })
-        .collect();
-    let lowercase_word = word.to_lowercase();
-    let mut aux: Vec<char> = lowercase_word.chars().collect();
-    aux.sort_unstable();
-    let lowercase_and_sorted_word: String = aux.into_iter().collect();
+        .filter(|item| {
+            // The closure passed to filter takes a reference to the item
+            // which means that item is a &&str
+            let item_lowercased = item.to_lowercase();
+            item_lowercased != word.to_lowercase() && sort_word(&item_lowercased) == sorted_word
+        }) // This returns an Iterator<Item = &&str>. We use copied to get an Iterator<Item = &str>
+        // see https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.filter
+        .copied() // This returns an Iterator<Item = &str>
+        .collect()
+}
 
-    let mut set: HashSet<&str> = HashSet::new();
-
-    for (i, item) in lowercase_and_sorted_possible_anagrams.iter().enumerate() {
-        if lowercase_and_sorted_word.eq(item) && lowercase_possible_anagrams[i].ne(&lowercase_word)
-        {
-            set.insert(possible_anagrams[i]);
-        }
-    }
-
-    set
+// Taken from here: https://stackoverflow.com/a/66289009/1715225
+fn sort_word(word: &str) -> Vec<char> {
+    let mut sorted_word: Vec<char> = word.chars().collect();
+    sorted_word.sort_unstable();
+    sorted_word
 }
