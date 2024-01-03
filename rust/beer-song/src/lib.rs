8a5b507def5db76db84dc2ba8286d2b1eaa6a6fa
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-03 06:39:53 +0100

    [rust/beer-song] First iteration

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1),
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    let mut song: String = String::from("");
+    for i in (end..=start).rev() {
+        let new_verse = verse(i);
+        song.push_str(&new_verse);
+        if i != end {
+            song.push_str("\n");
+        }
+    }
+    song
+}
