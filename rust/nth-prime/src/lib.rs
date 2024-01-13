1f680fc8e9a9cdea7df3983dbb2f6706058af270
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-13 10:32:03 +0100

    [rust/nth-prime] Second iteration

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,12 +1,14 @@
 pub fn nth(n: u32) -> u32 {
-    let mut primes: Vec<u32> = vec![2];
-    let mut index = 2;
-    while (primes.len() as u32) < (n + 1) {
-        match primes.iter().any(|prime| index % prime == 0) {
-            false => primes.push(index),
-            true => (),
-        }
-        index += 1;
-    }
-    *primes.last().unwrap()
+    let mut primes: Vec<u32> = vec![];
+    (2..)
+        .filter(|&candidate| {
+            if primes.iter().any(|prime| candidate % prime == 0) {
+                false
+            } else {
+                primes.push(candidate);
+                true
+            }
+        })
+        .nth(n as usize)
+        .unwrap()
 }
