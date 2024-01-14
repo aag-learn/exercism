b3b5de2383b4e94dd3e7ea6d602611e3196bc0d6
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-14 17:39:37 +0100

    [rust/prime-factors] Second iteration.

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn factors(n: u64) -> Vec<u64> {
+pub fn factors(mut n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = vec![];
     let mut factor: u64 = 2;
-    let mut aux = n;
-    while aux > 1 {
-        if aux % factor == 0 {
+    while n > 1 {
+        while n % factor == 0 {
             factors.push(factor);
-            aux /= factor;
-        } else {
-            factor += 1;
+            n /= factor;
         }
+        factor += 1;
     }
     factors
 }
