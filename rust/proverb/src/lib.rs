1eef1cc2725fc1c9c0b6ab25f3a8484ad7424dd6
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-20 09:12:21 +0100

    [rust/proverb] Second iteration.

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,13 +1,12 @@
+use core::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut proverb: Vec<String> = list
-        .windows(2)
-        .map(|x| format!("For want of a {} the {} was lost.", x[0], x[1]))
-        .collect();
-    if !list.is_empty() {
-        proverb.push(format!(
-            "And all for the want of a {}.",
-            list.first().unwrap()
-        ));
+    match list.first() {
+        None => String::new(),
+        Some(word) => list
+            .windows(2)
+            .map(|x| format!("For want of a {} the {} was lost.\n", x[0], x[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
     }
-    proverb.join("\n")
 }
