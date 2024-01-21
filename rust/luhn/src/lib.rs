9fc81ad4e53bda9800e11c1b643fca187eebeda2
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-21 20:51:48 +0100

    [rust/luhn] Third iteration: using try_fold and do everything in one pass.

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,25 +1,17 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let code_without_spaces: Vec<char> = code.chars().filter(|&x| !x.is_whitespace()).collect();
-
-    let result: Option<u32> = code_without_spaces
-        .iter()
-        .rev()
-        .enumerate()
-        .map(|(index, c)| match ((index + 1) % 2, c.to_digit(10)) {
-            (0, Some(x)) if x * 2 <= 9 => Some(x * 2),
-            (0, Some(x)) => Some((x * 2) - 9),
-            (_, Some(x)) => Some(x),
-            (_, None) => None,
-        })
-        .try_fold(0, |acc, digit| match (acc, digit) {
-            (acc, Some(digit)) => Some(acc + digit),
-            (_, None) => None,
-        });
+    let result: Option<(u32, u32)> =
+        code.chars()
+            .filter(|&x| !x.is_whitespace())
+            .rev()
+            .try_fold((0, 0), |(sum, count), val| match val.to_digit(10) {
+                None => None,
+                Some(x) if (count + 1) % 2 == 1 => Some((sum + x, count + 1)),
+                Some(x) if x * 2 > 9 => Some((sum + (x * 2 - 9), count + 1)),
+                Some(x) => Some((sum + x * 2, count + 1)),
+            });
     match result {
-        None => false,
-        Some(x) if x != 0 && x % 10 == 0 => true,
-        Some(x) if x == 0 && code_without_spaces.len() > 1 => true,
-        Some(_) => false,
+        Some((sum, count)) if count > 1 && sum % 10 == 0 => true,
+        _ => false,
     }
 }
