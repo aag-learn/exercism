1ba4a6486ce4a4abcc211ca6e2c658cf1f40ffad
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-21 13:36:02 +0100

    [rust/luhn] Second iteration: remove aux function

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -23,35 +23,3 @@ pub fn is_valid(code: &str) -> bool {
         Some(_) => false,
     }
 }
-pub fn is_valid_2(code: &str) -> bool {
-    let result: Vec<Option<u32>> = code
-        .chars()
-        .filter(|&x| x != ' ')
-        .rev()
-        .enumerate()
-        .map(|(index, c)| match ((index + 1) % 2, c.to_digit(10)) {
-            (0, Some(x)) if x * 2 <= 9 => Some(x * 2),
-            (0, Some(x)) => Some((x * 2) - 9),
-            (_, Some(x)) => Some(x),
-            (_, None) => None,
-        })
-        .collect();
-    println!("{:?}", result);
-    let result: Option<u32> = code
-        .chars()
-        .filter(|&x| x != ' ')
-        .rev()
-        .enumerate()
-        .map(|(index, c)| match ((index + 1) % 2, c.to_digit(10)) {
-            (0, Some(x)) if x * 2 <= 9 => Some(x * 2),
-            (0, Some(x)) => Some((x * 2) - 9),
-            (_, Some(x)) => Some(x),
-            (_, None) => None,
-        })
-        .fold(Some(0), |acc, digit| match (acc, digit) {
-            (Some(acc), Some(digit)) => Some(acc + digit),
-            (_, _) => None,
-        });
-    println!("{:?}", result);
-    false
-}
