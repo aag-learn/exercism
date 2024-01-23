08e43b3dfd4a1fb47aae175823233a6db9dbc4f4
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-23 06:55:40 +0100

    [rust/raindrops] Second iteration.

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,14 +1,16 @@
 pub fn raindrops(n: u32) -> String {
-    if n % 3 != 0 && n % 5 != 0 && n % 7 != 0 {
-        return n.to_string();
-    }
-
-    [3, 5, 7].iter().fold(String::from(""), |acc, &factor| {
+    let speak = [3, 5, 7].iter().fold(String::from(""), |acc, &factor| {
         match (factor, n % factor) {
             (3, 0) => acc + "Pling",
             (5, 0) => acc + "Plang",
             (7, 0) => acc + "Plong",
             _ => acc,
         }
-    })
+    });
+
+    if speak.is_empty() {
+        n.to_string()
+    } else {
+        speak
+    }
 }
