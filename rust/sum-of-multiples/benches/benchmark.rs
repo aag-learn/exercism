9346ad85e86cb639ae9f3d9d87076f4e794664ba
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-02-02 08:24:48 +0100

    [rust/sum-of-multiples] Second iteration...
    Adding benchmark and implementation using HashSet

---
+++ rust/sum-of-multiples/benches/benchmark.rs
@@ -0,0 +1,33 @@
+#![feature(test)]
+extern crate sum_of_multiples;
+extern crate test;
+
+use test::Bencher;
+
+#[bench]
+fn bench_tiny_hashset(b: &mut Bencher) {
+    let factors = &[2, 3];
+    let limit = 10;
+    b.iter(|| sum_of_multiples::sum_of_multiples_hashset(limit, factors));
+}
+
+#[bench]
+fn bench_tiny_vector(b: &mut Bencher) {
+    let factors = &[2, 3];
+    let limit = 10;
+    b.iter(|| sum_of_multiples::sum_of_multiples_vector(limit, factors));
+}
+
+#[bench]
+fn bench_mid_hashset(b: &mut Bencher) {
+    let factors = &[2, 3, 5, 7, 11];
+    let limit = 10000;
+    b.iter(|| sum_of_multiples::sum_of_multiples_hashset(limit, factors));
+}
+
+#[bench]
+fn bench_mid_vector(b: &mut Bencher) {
+    let factors = &[2, 3, 5, 7, 11];
+    let limit = 10000;
+    b.iter(|| sum_of_multiples::sum_of_multiples_vector(limit, factors));
+}
--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,6 +1,20 @@
 use itertools::Itertools;
+use std::collections::HashSet;
 
+// Provide two implementations, one using a vector an another one using
+// a HashSet. I was curious about which of the two might perform better,
+// so I also added some bechmarks. Apparently, the HashSet version performs
+// slightly better:
+// test bench_mid_hashset  ... bench:     267,133 ns/iter (+/- 9,473)
+// test bench_mid_vector   ... bench:     321,310 ns/iter (+/- 16,205)
+// test bench_tiny_hashset ... bench:         139 ns/iter (+/- 5)
+// test bench_tiny_vector  ... bench:         215 ns/iter (+/- 30)
+//
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    sum_of_multiples_hashset(limit, factors)
+}
+
+pub fn sum_of_multiples_vector(limit: u32, factors: &[u32]) -> u32 {
     let mut answer: Vec<u32> = vec![];
 
     for factor in factors.iter().filter(|&&x| x > 0) {
@@ -12,4 +26,16 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
         );
     }
     answer.iter().unique().sum()
+}
+
+pub fn sum_of_multiples_hashset(limit: u32, factors: &[u32]) -> u32 {
+    let mut answers = HashSet::new();
+    factors.iter().filter(|&&x| x > 0).for_each(|factor| {
+        (1..=(limit / factor)).for_each(|x| {
+            if x * factor < limit {
+                answers.insert(x * factor);
+            };
+        });
+    });
+    answers.iter().sum()
 }
