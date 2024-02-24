3fcbacffda47fe91f818c53e6731d6c54d436a20
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-02-24 08:28:50 +0100

    [ruby/protein-translation] Second iteration using each_slice and with_object

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -20,29 +20,14 @@ module Translation
   }.freeze
 
   def self.of_rna(strand)
-    results = []
-    results if strand.size < 3
+    [] if strand.size < 3
 
-    last = strand.size - 1
-    start_at = 0
-    end_at = 2
+    strand.chars.each_slice(3).with_object([]) do |codon, r|
+      protein = MAP.fetch(codon.join) { |key| raise InvalidCodonError, "Unknown codon: #{key}" }
+      return r if protein == 'STOP'
 
-    while end_at <= strand.size
-
-      codon = strand[start_at..end_at]
-      protein = MAP.fetch(codon) { |key| raise InvalidCodonError, "Unknown codon: #{key}" }
-      break if protein == 'STOP'
-
-      results.push(MAP[codon])
-
-      remaining = last - end_at
-      raise InvalidCodonError, 'Incomplete sequence' if remaining.positive? && remaining < 3
-
-      start_at = end_at + 1
-      end_at += 3
+      r.push(protein)
     end
-
-    results
   end
 end
 
