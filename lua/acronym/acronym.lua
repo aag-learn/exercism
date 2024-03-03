546523f68a735c50cedfcf706c403ed4e7f1d131
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-03-03 09:42:22 +0100

    [lua/acronym] Third iteration inspired in... https://exercism.org/tracks/lua/exercises/acronym/solutions/ryanplusplus

--- lua/acronym/acronym.lua
+++ lua/acronym/acronym.lua
@@ -1,13 +1,9 @@
 return function(s)
-	local words = s:gmatch("[%a]+")
-	local result = ""
-	for word in words do
-		result = result .. word:sub(1, 1):upper()
-		if not word:match("^%u+$") then
-			for letter in word:sub(2):gmatch("%u") do
-				result = result .. letter:upper()
-			end
-		end
-	end
-	return result
+	-- Solution inspired in the one by ryanplusplus
+	-- https://exercism.org/tracks/lua/exercises/acronym/solutions/ryanplusplus
+	local acronym = {}
+	s:gsub("%u*%l*", function(match)
+		table.insert(acronym, match:sub(1, 1):upper())
+	end)
+	return table.concat(acronym, "")
 end
