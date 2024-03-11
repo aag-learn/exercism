7f0545a5ba194052faa951cb52ebd02f7ea45dd6
Alfonso Alba García <alfonso@rubiconmd.com>
2024-03-11 07:20:41 -0400

    [rust/high-scores] First Iteration...

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,26 @@
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut data = Vec::from(self.scores);
+        data.sort();
+        data.iter().rev().take(3).cloned().collect()
+    }
+}
