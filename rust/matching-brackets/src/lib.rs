edcf848f8323ebe5b9383bd0aa7a2dbf913f6b11
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-03-24 11:46:44 +0100

    [rust/matching-brackets] Working on second iteration

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,46 @@
 const BRACKETS: [char; 6] = ['[', ']', '(', ')', '{', '}'];
+const OPENING_BRACKETS: [char; 3] = ['[', '(', '{'];
+const CLOSING_BRACKETS: [char; 3] = [']', ')', '}'];
 
 pub fn brackets_are_balanced(string: &str) -> bool {
+    // replacing_matching_brackets(string)
+    let mut stack = vec![];
+    for c in string.chars() {
+        if BRACKETS.contains(&c) {
+            match c {
+                x if OPENING_BRACKETS.contains(&x) => stack.push(c),
+                x if CLOSING_BRACKETS.contains(&x) => match x {
+                    ']' => {
+                        if stack.last() == Some(&'[') {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                    ')' => {
+                        if stack.last() == Some(&'(') {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                    '}' => {
+                        if stack.last() == Some(&'{') {
+                            stack.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                    _ => (),
+                },
+                _ => (),
+            }
+        }
+    }
+    stack.is_empty()
+}
+
+pub fn replacing_matching_brackets(string: &str) -> bool {
     let mut brackets: String = string.chars().filter(|c| BRACKETS.contains(c)).collect();
 
     while !brackets.is_empty()
