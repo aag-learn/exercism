20aee746a977461a820e11548fc32597dd1607f2
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-05-19 19:28:01 +0200

    [ruby/allergies]: Starting point

---
+++ ruby/allergies/allergies.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Allergies' exercise in this file. Make the tests in
+`allergies_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/allergies` directory.
+=end
