675c883b93440feabf47e0ce88a7630bd0df67b8
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-05-19 20:39:15 +0200

    [ruby/allergies] Second iteration

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,26 +1,18 @@
+# Using an array instead of a hash to store the allergens
 class Allergies
-  ALLERGENS = {
-    eggs: 1,
-    peanuts: 2,
-    shellfish: 4,
-    strawberries: 8,
-    tomatoes: 16,
-    chocolate: 32,
-    pollen: 64,
-    cats: 128
-  }.freeze
+  ALLERGENS = %w[eggs peanuts shellfish strawberries tomatoes chocolate pollen cats].freeze
 
   attr_reader :score
 
   def initialize(score)
-    @score = score
+    @score = score.digits(2)
   end
 
   def allergic_to?(allergen)
-    score & ALLERGENS[allergen.to_sym] != 0
+    score[ALLERGENS.index(allergen)] == 1
   end
 
   def list
-    ALLERGENS.keys.select { allergic_to? _1 }.map(&:to_s)
+    ALLERGENS.select { allergic_to? _1 }
   end
 end
