638a1f06de57f5c9a1cc49a1a8cb591dc86e55d9
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-05-19 20:20:10 +0200

    [ruby/allergies]: First iteration

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,7 +1,26 @@
-=begin
-Write your code for the 'Allergies' exercise in this file. Make the tests in
-`allergies_test.rb` pass.
+class Allergies
+  ALLERGENS = {
+    eggs: 1,
+    peanuts: 2,
+    shellfish: 4,
+    strawberries: 8,
+    tomatoes: 16,
+    chocolate: 32,
+    pollen: 64,
+    cats: 128
+  }.freeze
 
-To get started with TDD, see the `README.md` file in your
-`ruby/allergies` directory.
-=end
+  attr_reader :score
+
+  def initialize(score)
+    @score = score
+  end
+
+  def allergic_to?(allergen)
+    score & ALLERGENS[allergen.to_sym] != 0
+  end
+
+  def list
+    ALLERGENS.keys.select { allergic_to? _1 }.map(&:to_s)
+  end
+end
