ab2ffccc500c61cbb6f305fe561cc418bf26211e
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-05-26 18:25:06 +0200

    [ruby/chess-game] First iteration

--- ruby/chess-game/chess_game.rb
+++ ruby/chess-game/chess_game.rb
@@ -1,16 +1,22 @@
+# frozen_string_literal: true
+
 module Chess
-  # TODO: define the 'RANKS' constant
-  # TODO: define the 'FILES' constant
+  RANKS = 1..8
+  FILES = 'A'..'H'
 
   def self.valid_square?(rank, file)
-    raise "Please implement the Chess.valid_square? method"
+    RANKS.include?(rank.to_i) && FILES.include?(file.to_s)
   end
 
   def self.nick_name(first_name, last_name)
-    raise "Please implement the Chess.nick_name method"
+    "#{first_name[0, 2].upcase}#{last_name[-2..].upcase}"
   end
 
   def self.move_message(first_name, last_name, square)
-    raise "Please implement the Chess.move_message method"
+    if valid_square?(square[1], square[0])
+      "#{nick_name(first_name, last_name)} moved to #{square}"
+    else
+      "#{nick_name(first_name, last_name)} attempted to move to #{square}, but that is not a valid square"
+    end
   end
 end
