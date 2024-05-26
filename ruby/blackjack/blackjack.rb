ddb7b81f319dd367c521036bbbf1589cfcecbb10
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-05-26 19:41:37 +0200

    [ruby/blackjack] First iteration

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,13 +1,47 @@
+# frozen_string_literal: true
+
 module Blackjack
+  CARDS = {
+    ace: 11,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10,
+    jack: 10,
+    queen: 10,
+    king: 10
+  }
   def self.parse_card(card)
-    raise "Please implement the Blackjack.parse_card method"
+    CARDS[card.to_sym] || 0
   end
 
   def self.card_range(card1, card2)
-    raise "Please implement the Blackjack.card_range method"
+    sum = CARDS[card1.to_sym] + CARDS[card2.to_sym]
+    case sum
+    when (4..11) then 'low'
+    when (12..16) then 'mid'
+    when (17..20) then 'high'
+    when 21 then 'blackjack'
+    end
   end
 
   def self.first_turn(card1, card2, dealer_card)
-    raise "Please implement the Blackjack.first_turn method"
+    return 'P' if card1 == 'ace' && card2 == 'ace'
+
+    case card_range(card1, card2)
+    when 'blackjack'
+      parse_card(dealer_card) < 10 ? 'W' : 'S'
+    when 'high'
+      'S'
+    when 'mid'
+      parse_card(dealer_card) < 7 ? 'S' : 'H'
+    when 'low'
+      'H'
+    end
   end
 end
