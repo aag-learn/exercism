3c42aada5fcdfa9f339b37c715cfdd9c906da42e
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-06-29 09:55:30 +0200

    [bash/acronym] Second iteration

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 main() {
+	# using_sed "$@"
+	using_bash "$@"
+}
+
+# Solution using parameter expansion
+using_bash() {
+	local acronym=""
+	local sentence=${@//[^a-zA-Z0-9\']/ }
+	for word in ${sentence}; do
+		acronym="${acronym}${word:0:1}"
+	done
+	echo ${acronym^^}
+}
+
+using_sed() {
 	local words=($(echo "$@" | sed -E "s/[^a-zA-Z0-9']+/ /g"))
 	local result=""
 	for word in "${words[@]}"; do
