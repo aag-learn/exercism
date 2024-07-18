58014724807987bed34dcbc30060b91479c72b59
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-07-18 07:27:56 +0200

    [ruby/sieve] Second Iteration

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -6,6 +6,10 @@ class Sieve
   end
 
   def primes
+    primes_iteration_2
+  end
+
+  def primes_iteration_1
     test = (0..@number).to_a
     primes = []
     (2..@number).each do |index|
@@ -17,6 +21,17 @@ class Sieve
         test[cursor * index] = 0
         cursor += 1
       end
+    end
+    primes
+  end
+
+  def primes_iteration_2
+    test = (2..@number).to_a
+    primes = []
+    while test.any?
+      number = test.shift
+      primes.push number
+      test = test.select { |x| (x % number).positive? }
     end
     primes
   end
