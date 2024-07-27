78e21744b5c8adc21560fa7a89a1ab3169f0dfff
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-07-27 11:15:05 +0200

    [lua/sieve] First iteration

--- lua/sieve/sieve.lua
+++ lua/sieve/sieve.lua
@@ -1,2 +1,17 @@
 return function(n)
+	local co = coroutine.create(function()
+		local test = {}
+		for i = 2, n do
+			test[i] = true
+		end
+		for i = 2, n do
+			if test[i] then
+				for j = i * i, n, i do
+					test[j] = false
+				end
+				coroutine.yield(i)
+			end
+		end
+	end)
+	return co
 end
