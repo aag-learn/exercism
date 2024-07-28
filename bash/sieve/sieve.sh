734e5598a911335b4c34cb491b7ba9ebae4eca10
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-07-28 09:43:02 +0200

    [bash/sieve] Second iteration
    - Remove $ from arithmetic variables
    - Remove comment

--- bash/sieve/sieve.sh
+++ bash/sieve/sieve.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
-
 main() {
   local n=$1
   [ "$n" == 1 ] && echo "" && exit 0
@@ -31,8 +8,8 @@ main() {
   for test in $(seq 2 "$n"); do
     if [ "${numbers[$test]}" != "false" ]; then
       primes+=("$test")
-      for ((i = $test; i <= $n; i += $test)); do
-        numbers[$i]="false"
+      for ((i = test; i <= n; i += test)); do
+        numbers[i]="false"
       done
     fi
   done
