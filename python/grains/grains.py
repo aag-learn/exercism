d0e57d1d3b42009d115a82b3ccacf5837d254723
Alfonso Alba García <alfonso@alfonsoalba.com>
2026-06-29 01:25:11 +0200

    [python/grains] Second iteration

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,16 +1,36 @@
+"""
+Helps the king calculate how many grains he need to pay to
+his servant
+"""
+
 def square(number):
+    """
+    Returns the number of grains in a particular square of the chess board, 
+    given that the first square has one grain and each subsequent square 
+    has double the grains of the previous square.
+
+    parameter 
+      number: int - the square number (1-64)
+
+    returns
+      int - the number of grains on the specified square
+    """
     if number < 1 or number > 64:
         raise ValueError("square must be between 1 and 64")
 
-    if number in (1, 2):
+    if number in {1, 2}:
         return number
 
     return 2 * square(number - 1)
 
 
 def total():
+    """
+    Returns the totla number of grains on the chess board, which is the sum 
+    of grains on all 64 squares.
+    """
     accumulated = 0
-    for i in range(1, 65):
-        accumulated += square(i)
+    for cursor in range(1, 65):
+        accumulated += square(cursor)
 
     return accumulated
