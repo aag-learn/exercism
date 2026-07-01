f35a92c9b07c470cd16e7b388e8ba0a8f1e604c3
Alfonso Alba García <alfonso@alfonsoalba.com>
2026-07-02 01:24:39 +0200

    [python/triangle] Third iteration: use decorators
    Learnt about decorators in the community solution
    https://exercism.org/tracks/python/exercises/triangle/solutions/paiv

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -2,49 +2,44 @@
     Triangle validations module
 """
 
+def valid_triangle(f):
+    """
+    Decorator to verify that provided sides can form a triangle.
+    """
+    def inner(sides):
+        (a,b,c) = sides
+
+        if a == b == c == 0.0:
+            return False
+
+        return (a + b >= c) and (b + c >= a) and (a + c >= b) and f(sides)
+
+    return inner
+
+@valid_triangle
 def equilateral(sides):
     """
     Returns True if the triangle is equilateral, False otherwise.
     """
-    if not is_a_triangle(sides):
-        return False
 
     (a,b,c) = sides
     return a == b == c
 
+@valid_triangle
 def isosceles(sides):
     """
     Returns True if the triangle is isosceles, False otherwise.
     """
 
-    if not is_a_triangle(sides):
-        return False
-
     (a,b,c) = sides
     return a == b or a == c or b == c
     # return len(set(sides)) <= 2
 
+@valid_triangle
 def scalene(sides):
     """
     Returns True if the triangle is scalene, False otherwise.
     """
 
-    if not is_a_triangle(sides):
-        return False
-
     (a,b,c) = sides
     return a != b and a != c and b != c
-
-def is_a_triangle(sides):
-    """
-    Returns True if the sides can form a triangle, False otherwise.
-
-    For example, if all sides are zero, it is not a triangle. If the sum of any two sides is less than the third side, it is not a triangle.
-    """
-
-    (a,b,c) = sides
-
-    if a == b == c == 0.0:
-        return False
-
-    return (a + b >= c) and (b + c >= a) and (a + c >= b)
