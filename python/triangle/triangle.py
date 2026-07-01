c627350297836e680852c421d0c9c8f404fd39b2
Alfonso Alba García <alfonso@alfonsoalba.com>
2026-07-02 01:35:03 +0200

    [python/triangle] Fourthiteration: use sets
    Use set to simplifu the way we calculate each of those trangles

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -7,12 +7,7 @@ def valid_triangle(f):
     Decorator to verify that provided sides can form a triangle.
     """
     def inner(sides):
-        (a,b,c) = sides
-
-        if a == b == c == 0.0:
-            return False
-
-        return (a + b >= c) and (b + c >= a) and (a + c >= b) and f(sides)
+        return sum(sides) > 2 * max(sides) and f(sides)
 
     return inner
 
@@ -22,8 +17,7 @@ def equilateral(sides):
     Returns True if the triangle is equilateral, False otherwise.
     """
 
-    (a,b,c) = sides
-    return a == b == c
+    return len(set(sides)) == 1
 
 @valid_triangle
 def isosceles(sides):
@@ -31,15 +25,12 @@ def isosceles(sides):
     Returns True if the triangle is isosceles, False otherwise.
     """
 
-    (a,b,c) = sides
-    return a == b or a == c or b == c
-    # return len(set(sides)) <= 2
+    return len(set(sides)) <= 2
 
 @valid_triangle
 def scalene(sides):
     """
     Returns True if the triangle is scalene, False otherwise.
     """
+    return len(set(sides)) == 3
 
-    (a,b,c) = sides
-    return a != b and a != c and b != c
