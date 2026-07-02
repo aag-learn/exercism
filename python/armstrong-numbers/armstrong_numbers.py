cdfc0168f83b97810c48ab8bf509de0f6eee18a6
Alfonso Alba García <alfonso@alfonsoalba.com>
2026-07-03 01:45:30 +0200

    [python/armstrong_number] Second iteration: fix lint errors

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,4 +1,12 @@
+"""
+Helper methods to work with Armstrong numbers
+"""
+
+
 def is_armstrong_number(number):
+    """
+    Returns True if a number is an armstrong number.
+    """
     number_of_digits = len(str(number))
     result = 0
     for digit in str(number):
