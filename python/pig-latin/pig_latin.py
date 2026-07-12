889c85202ab6269ffcc3f4c4f8f226c5170587cc
Alfonso Alba García <alfonso@alfonsoalba.com>
2026-07-13 00:46:16 +0200

    [python/pig-latin] First iteration

---
+++ python/pig-latin/pig_latin.py
@@ -0,0 +1,30 @@
+"""
+Pig latin translation utilities
+"""
+
+import re
+
+RULE_1_PREFIXES = ("a", "e", "i", "o", "u", "xr", "yt")
+RULE_2_PATTERN = r"^([b-df-hj-np-tvwxyz]+)(.*)"
+RULE_3_PATTERN = r"^([b-df-hj-np-tvwxz]*qu)(.*)"
+RULE_4_PATTERN = r"^([b-df-hj-np-tvwxz]+)(y.*)"
+
+
+def translate(text):
+
+    words = text.split(" ")
+    translated_words = []
+    for word in words:
+        if word.startswith(RULE_1_PREFIXES):
+            translated_words.append(word + "ay")
+            continue
+
+        match = (
+            re.match(RULE_3_PATTERN, word)
+            or re.match(RULE_4_PATTERN, word)
+            or re.match(RULE_2_PATTERN, word)
+        )
+        if match:
+            translated_words.append(match.group(2) + match.group(1) + "ay")
+
+    return " ".join(translated_words)
