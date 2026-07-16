608d9787a68570a9240d9a03942b368d4b22c992
Alfonso Alba García <alfonso@alfonsoalba.com>
2026-07-17 01:48:14 +0200

    [python/little-sisters-vocab] First iteration

---
+++ python/little-sisters-vocab/strings.py
@@ -0,0 +1,91 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+
+import re
+
+NESS_PATTERN = re.compile(r"[^aeiou]i$")
+
+
+def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    Parameters:
+        word (str): The root word.
+
+    Returns:
+        str: Root word prepended with 'un'.
+    """
+
+    return f"un{word}"
+
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words.
+
+    Parameters:
+        vocab_words (list[str]): Vocabulary words with prefix at first index.
+
+    Returns:
+        str: Prefix followed by vocabulary words with prefix applied.
+
+    This function takes a `vocab_words` list of strings and returns a string
+    with the prefix and the words with prefix applied, separated by ' :: '.
+
+    Examples:
+        >>> list('en', 'close', 'joy', 'lighten')
+        'en :: enclose :: enjoy :: enlighten'.
+    """
+
+    prefix = vocab_words[0]
+    words = vocab_words[1:]
+
+    results = [prefix] + [f"{prefix}{word}" for word in words]
+    return " :: ".join(results)
+
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    Parameters:
+        word (str): Word to remove suffix from.
+
+    Returns:
+        str: Word with suffix removed & spelling adjusted.
+
+    Examples:
+        >>> remove_suffix_ness('heaviness')
+        'heavy'
+
+        >>> remove_suffix_ness('sadness')
+        'sad'
+
+    """
+
+    word_without_suffix = word.replace("ness", "")
+    print(word_without_suffix)
+    if NESS_PATTERN.search(word_without_suffix):
+        word_without_suffix = word_without_suffix[:-1] + "y"
+    return word_without_suffix
+
+
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    Parameters:
+        sentence (str): The word used in a sentence as an adjective.
+        index (int): Index of the adjective to remove and transform.
+
+    Returns:
+        str: The extracted adjective in verb form.
+
+    Examples:
+        >>> adjective_to_verb('It got dark as the sun set.', 2)
+        'darken'
+
+        >>> adjective_to_verb('The ink stains her fingers black.', -1)
+        'blacken'
+
+    """
+
+    words = sentence[:-1].split()
+    adjective = words[index]
+    return f"{adjective}en"
